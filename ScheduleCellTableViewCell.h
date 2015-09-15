c1f0e3a22c349266f48520c5577565bdc04c9c72
Basanth Verma <basanth1994@gmail.com>
2015-09-15 13:03:52 +0530

    Create ScheduleCellTableViewCell.h
    header File for ScheduleCellTableViewCell.m

---
+++ ScheduleCellTableViewCell.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+@interface ScheduleCellTableViewCell : UITableViewCell 
+@property(nonatomic,retain)UILabel *customerName;
+@property(nonatomic,retain)UILabel *serviceBooked;
+
+
+
+@end
